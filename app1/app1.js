2676ecbf796c7d9d96a43f98d9676c223070e895
Abby Liang <abby.liang@42-q.com>
2022-11-24 11:50:28 +0800

    update register application

--- app1/app1.js
+++ app1/app1.js
@@ -1,9 +1,10 @@
 System.register([], function (_export, _moduleContext) {
+  let domEl;
 
   function bootstrap(props) {
-    console.log(props);
+    // console.log(props);
     return Promise.resolve().then(() => {
-      const domEl = document.createElement("div");
+      domEl = document.createElement("div");
       domEl.textContent = props.name + " is mounted!";
       domEl.id = props.name+Date.now();
       
@@ -16,13 +17,18 @@ System.register([], function (_export, _moduleContext) {
 
   function mount(props) {
     return Promise.resolve().then(() => {
-      // domEl.textContent = props.name + " is mounted!";
+      domEl.textContent = props.name + " is mounted!";
+      
     });
   }
 
   function unmount(props) {
     return Promise.resolve().then(() => {
-      domEl.textContent = "";
+      const rootEl = document.getElementById(`single-spa-application:${props.name}`)
+      console.log('unmounted');
+      console.log(props);
+      document.body.removeChild(domEl);
+      document.body.removeChild(rootEl);
     });
   }
   _export({ bootstrap, mount, unmount });
--- single-spa-config.js
+++ single-spa-config.js
@@ -1,20 +1,67 @@
+// System.import('single-spa').then(singleSpa=>{
+//     singleSpa.registerApplication(
+//         "app",
+//         () => System.import("app"),
+//         ()=> location.pathname.startsWith('/')
+//     );
+//     // singleSpa.registerApplication(
+//     //     "app2",
+//     //     () => System.import("app2"),
+//     //     ()=> location.pathname.startsWith('/')
+//     // );
+//     singleSpa.registerApplication(
+//         "app3",
+//         () => System.import("app"),
+//         ()=> {
+//             console.log('test');
+//             return location.pathname.startsWith('/app')
+//         }
+//     );
 
-System.import('single-spa').then(singleSpa=>{
-    singleSpa.registerApplication(
-        "app",
-        () => System.import("app"),
-        ()=> location.pathname.startsWith('/')
-    );
-    singleSpa.registerApplication(
-        "app2",
-        () => System.import("app2"),
-        ()=> location.pathname.startsWith('/')
-    );
-    singleSpa.registerApplication(
-        "app3",
-        () => System.import("app"),
-        ()=> location.pathname.startsWith('/')
-    );
-    
-    singleSpa.start();
-})
+//     singleSpa.start();
+// })
+
+
+function registerApplication(singleSpa, appName, path) {
+  singleSpa.registerApplication(
+    appName,
+    () => System.import(appName),
+    () => location.pathname.startsWith(path)
+  );
+  
+}
+
+
+function unregisterApp(singleSpa, app){
+    singleSpa.unregisterApplication(app).then(_=>{
+        console.log('unregister app');
+        console.log(app);
+    })
+
+}
+
+(
+ async function(){
+    try {
+        const singleSpa = await System.import('single-spa');
+        singleSpa.start();
+        const app = document.querySelector('#app');
+        const detach = document.querySelector('#detach');
+        
+        app.onclick = function(){
+            registerApplication(singleSpa,'app','/');
+            registerApplication(singleSpa,'app2','/app');
+            this.setAttribute('disabled','true');
+            detach.removeAttribute('disabled');
+        }
+        detach.onclick = function(){
+            unregisterApp(singleSpa, 'app');
+            unregisterApp(singleSpa, 'app2');
+            this.setAttribute('disabled','true');
+            app.removeAttribute('disabled');
+        }
+    } catch (error) {
+        console.log('err', error);
+    }
+ }
+)()
